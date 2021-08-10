1b9eedcd1abdcbfafc0fc4e5a8c0473e25901daa
anonymous anonymous <raabia@chicagobooth.edu>
2021-08-10 14:54:30 +0000

    Generate actors resource with ideas:scaffold

---
+++ db/migrate/20210810145428_create_actors.rb
@@ -0,0 +1,11 @@
+class CreateActors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :actors do |t|
+      t.string :name
+      t.text :bio
+      t.date :dob
+
+      t.timestamps
+    end
+  end
+end
